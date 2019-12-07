9c4dbd14e999d51eeaa178b7fc138110de961529
Frederico T <frederico.thofehrn@acad.pucrs.br>
2019-12-07 14:21:52 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">pessoas Front</a>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <form>
+
+          <label>Nome</label>
+          <input type="text" placeholder="Nome">
+          <label>Email</label>
+          <input type="text" placeholder="Email">
+
+          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>NOME</th>
+            <th>EMAIL</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          <tr>
+
+            <td>Juliana</td>
+            <td>juliana@email.com</td>
+            <td>
+              <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+              <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+            </td>
+
+          </tr>
+
+        </tbody>
+      
+      </table>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import pessoas from './services/pessoas'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+export default{
+  mounted() {
+    pessoas.listar().then(resposta=>{
+      console.log(resposta)
+    })
   }
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
